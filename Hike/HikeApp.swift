3044d18816e0f73eb5145ae99378eb88ccd853a7
petar.iliev <petar.iliev@servicenow.com>
2023-06-25 08:41:13 -0700

    Initial Commit

---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by Petar Iliev on 6/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
